00a196d63313e2cac0f97a3acc0871a70ed91ccc
adityasan92 <adityasan92@gmail.com>
2016-05-21 17:16:32 -0400

    list component

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import Home from './home/home';
 import About from './about/about';
+import List from './list/list';
 
 let componentModule = angular.module('app.components', [
   Home.name,
-  About.name
+  About.name,
+  List.name
 ]);
 
 export default componentModule;
---
+++ client/app/components/list/list.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import uiRouter from 'angular-ui-router';
+import listComponent from './list.component';
+
+let listModule = angular.module('list', [
+  uiRouter
+])
+
+.component('list', listComponent);
+
+export default listModule;
