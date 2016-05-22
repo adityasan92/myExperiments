272c09b2abe745171710a90698a86ea70a4e790f
adityasan92 <adityasan92@gmail.com>
2016-05-21 22:44:09 -0400

    clear functionality

--- client/app/components/drawcard/drawcard.controller.js
+++ client/app/components/drawcard/drawcard.controller.js
@@ -45,6 +45,11 @@ class DrawcardController {
     console.log(image);
   }
 
+  clear(){
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
 }
 
 export default DrawcardController;
