fbe0d2279722c5daebd60b9bfc41b283c08cd1ba
Aditya Sanghi <adityasan92@gmail.com>
2016-05-23 20:54:21 -0400

    save varibles try

--- ml/SingleDigitClas/sdc.py
+++ ml/SingleDigitClas/sdc.py
@@ -6,7 +6,7 @@ print vars(mnist)
 x = tf.placeholder(tf.float32, [None, 784])
 y_ = tf.placeholder(tf.float32, [None, 10])
 
-sess = tf.InteractiveSession()
+#sess = tf.InteractiveSession()
 
 def weight_variable(shape):
 	initial =  tf.truncated_normal(shape, stddev=0.1)
@@ -54,14 +54,21 @@ cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_ind
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv,1), tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-sess.run(tf.initialize_all_variables())
-for i in range(20000):
-  batch = mnist.train.next_batch(50)
-  if i%100 == 0:
-    train_accuracy = accuracy.eval(feed_dict={
-        x:batch[0], y_: batch[1], keep_prob: 1.0})
-    print("step %d, training accuracy %g"%(i, train_accuracy))
-  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+init_op = tf.initialize_all_variables()
+saver = tf.train.Saver()
+
+with tf.Session() as sess:
+	sess.run(init_op)
+	for i in range(200):
+	  batch = mnist.train.next_batch(50)
+	  if i%100 == 0:
+	    train_accuracy = accuracy.eval(feed_dict={
+	        x:batch[0], y_: batch[1], keep_prob: 1.0})
+	    print("step %d, training accuracy %g"%(i, train_accuracy))
+	  train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+	save_path = saver.save(sess, "/tmp/model.ckpt")
+	print("MOdel saved in fileL %s" % save_path)
+	print("test accuracy %g"%accuracy.eval(feed_dict={
+	    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
